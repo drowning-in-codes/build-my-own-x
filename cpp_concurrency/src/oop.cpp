82248c0876177b510823e330ab89b9b51e6e8418
proanimer <bukalala174@gmail.com>
2024-10-29 09:19:26 +0800

    undefined Add css learning

--- cpp_concurrency/src/oop.cpp
+++ cpp_concurrency/src/oop.cpp
@@ -6,6 +6,7 @@ class Base {
 public:
   Base() { cout << "Base constructor" << endl; }
   Base(int n) : num(n) { cout << "Base constructor" << n << endl; }
+  virtual void sayHi() { std::cout << "Base sayHi" << std::endl; }
   virtual ~Base() { cout << "Base destructor" << endl; }
   int getNum() { return num; }
 
@@ -17,12 +18,15 @@ class Derived : public Base {
 public:
   Derived() { cout << "Derived constructor" << endl; }
   Derived(int n) { cout << "Derived constructor" << n << endl; }
+  void sayHi() override { std::cout << "Derived sayHi" << std::endl; }
+
   ~Derived() override { cout << "Derived destructor" << endl; }
 };
 int f() { return 5; }
 int main() {
   const int a = f();
   Base *b = new Derived();
+  b->sayHi();
   delete b;
   Derived d{1};
   return 0;
